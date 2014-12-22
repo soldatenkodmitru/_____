64b788865ec2f2c41a7318e13f4918702051062d
gavrilak <gavrilak89@gmail.com>
2014-12-22 13:45:07 +0200

    Initial Commit

---
+++ Ringtones/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Ringtones
+//
+//  Created by Дима on 22.12.14.
+//  Copyright (c) 2014 BestAppStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Ringtones/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ringtones
+//
+//  Created by Дима on 22.12.14.
+//  Copyright (c) 2014 BestAppStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
